bf0169b5135ec82fb21a8e3943a7804e92ea154e
silvajpedro <belojpedro.31@gmail.com>
2024-11-25 09:55:20 -0300

    tirei o conteudo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,59 +9,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <BrowserRouter>
-      <header>
-        <section className={s.logoHeader}>
-          <Link to="/">
-            <img
-              src={logoLivro}
-              alt="Imagem de um livro azul aberto, com as paginas sendo trocadas, logo do projeto"
-            />
-          </Link>
-
-          <h1>Livros Vai Na Web</h1>
-        </section>
-
-        <nav className={s.navHeader}>
-          <ul>
-            <li>
-              <Link className={s.link} to="/">
-                Início
-              </Link>
-            </li>
-            <li>
-              <Link className={s.link} to="/livrosDoados">
-                Livros Doados
-              </Link>
-            </li>
-            <li>
-              <Link className={s.link} to="/queroDoar">
-                Quero Doar
-              </Link>
-            </li>
-          </ul>
-        </nav>
-
-        <section className={s.barraDeBusca}>
-          <input
-            type="search"
-            name=""
-            id=""
-            placeholder="O que você procura?"
-          />
-          <button>
-            <img
-              src={lupaHeader}
-              alt="Imagem de uma lupa branca com serventia de ser um botão para pesquisa"
-            />
-          </button>
-        </section>
-      </header>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/livrosDoados" element={<LivrosDoados />} />
-        <Route path="/queroDoar" element={<QueroDoar />} />
-      </Routes>
-    </BrowserRouter>
+    <h1>aaaaa</h1>
   );
 }
