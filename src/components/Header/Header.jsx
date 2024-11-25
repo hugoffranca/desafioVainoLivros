225683fed1c672c2ac0c4c85c7c3012877bb1489
silvajpedro <belojpedro.31@gmail.com>
2024-11-25 10:01:45 -0300

    novo teste

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,67 @@
+import logoLivro from "../../assets/logoLivro.png";
+import lupaHeader from "../../assets/lupaHeader.png";
+import Inicio from "../../pages/inicio/Inicio.jsx";
+import LivrosDoados from "../../pages/livrosDoados/LivrosDoados"
+import QueroDoar from "../../pages/queroDoar/QueroDoar.jsx"
+import s from "../header/header.module.scss";
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <h1>aaa</h1>
+    <BrowserRouter>
+      <header>
+        <section className={s.logoHeader}>
+          <Link to="/">
+            <img
+              src={logoLivro}
+              alt="Imagem de um livro azul aberto, com as paginas sendo trocadas, logo do projeto"
+            />
+          </Link>
+
+          <h1>Livros Vai Na Web</h1>
+        </section>
+
+        <nav className={s.navHeader}>
+          <ul>
+            <li>
+              <Link className={s.link} to="/">
+                Início
+              </Link>
+            </li>
+            <li>
+              <Link className={s.link} to="/livrosDoados">
+                Livros Doados
+              </Link>
+            </li>
+            <li>
+              <Link className={s.link} to="/queroDoar">
+                Quero Doar
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <section className={s.barraDeBusca}>
+          <input
+            type="search"
+            name=""
+            id=""
+            placeholder="O que você procura?"
+          />
+          <button>
+            <img
+              src={lupaHeader}
+              alt="Imagem de uma lupa branca com serventia de ser um botão para pesquisa"
+            />
+          </button>
+        </section>
+      </header>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/livrosDoados" element={<LivrosDoados />} />
+        <Route path="/queroDoar" element={<QueroDoar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
