b4a9412c61a8823c2000b1a20472ccc940c71b44
silvajpedro <belojpedro.31@gmail.com>
2024-11-25 09:58:18 -0300

    apaguei todo conteudo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,5 @@
-import logoLivro from "../../assets/logoLivro.png";
-import lupaHeader from "../../assets/lupaHeader.png";
-import Inicio from "../../pages/inicio/Inicio.jsx";
-import LivrosDoados from "../../pages/LivrosDoados/LivrosDoados"
-import QueroDoar from "../../pages/QueroDoar/QueroDoar.jsx"
-import s from "../header/header.module.scss";
-
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
 export default function Header() {
   return (
-    <h1>aaaaa</h1>
+    <h1>aaa</h1>
   );
 }
